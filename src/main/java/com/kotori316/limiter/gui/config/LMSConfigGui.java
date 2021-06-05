292ae06be504906d1c70f071d6223a7d37587d4b
Kotori316 <Kotori316@gmail.com>
2021-06-05 22:31:02 +0900

    Better suggestion for config GUI.

--- src/main/java/com/kotori316/limiter/gui/config/LMSConfigGui.java
+++ src/main/java/com/kotori316/limiter/gui/config/LMSConfigGui.java
@@ -1,24 +1,25 @@
 package com.kotori316.limiter.gui.config;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import com.feed_the_beast.mods.ftbguilibrary.config.ConfigGroup;
 import com.feed_the_beast.mods.ftbguilibrary.config.NameMap;
 import com.google.gson.JsonObject;
-import net.minecraft.entity.EntityClassification;
-import net.minecraft.entity.SpawnReason;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientSuggestionProvider;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 import com.kotori316.limiter.TestSpawn;
 import com.kotori316.limiter.capability.LMSHandler;
-import com.kotori316.limiter.conditions.DimensionLimit;
-import com.kotori316.limiter.conditions.EntityClassificationLimit;
-import com.kotori316.limiter.conditions.EntityLimit;
 import com.kotori316.limiter.conditions.PositionLimit;
-import com.kotori316.limiter.conditions.SpawnReasonLimit;
+import com.kotori316.limiter.conditions.StringLimitSerializer;
 
 public class LMSConfigGui {
 
@@ -30,14 +31,10 @@ public class LMSConfigGui {
 
     public static ConfigGroup createConfigGroup(JsonObject save, TestSpawn.Serializer<?> serializer) {
         ConfigGroup group = new ConfigGroup(serializer.getType());
-        if (serializer.equals(DimensionLimit.SERIALIZER) || serializer.equals(EntityLimit.SERIALIZER)) {
-            addStringConfig(save, group, serializer);
-        } else if (serializer.equals(EntityClassificationLimit.SERIALIZER)) {
-            addClassificationConfig(save, group, serializer);
-        } else if (serializer.equals(SpawnReasonLimit.SERIALIZER)) {
-            addReasonConfig(save, group, serializer);
-        } else if (serializer.equals(PositionLimit.SERIALIZER)) {
+        if (serializer.equals(PositionLimit.SERIALIZER)) {
             addPositionConfig(save, group, serializer);
+        } else {
+            addSinglePropertyConfig(save, group, serializer);
         }
         return group;
     }
@@ -48,17 +45,28 @@ public class LMSConfigGui {
         }
     }
 
-    private static void addClassificationConfig(JsonObject save, ConfigGroup group, TestSpawn.Serializer<?> serializer) {
+    @SuppressWarnings({"ConstantConditions"})
+    private static void addSinglePropertyConfig(JsonObject save, ConfigGroup group, TestSpawn.Serializer<?> serializer) {
+        ClientSuggestionProvider provider = Minecraft.getInstance().player.connection.getSuggestionProvider();
         for (String key : serializer.propertyKeys()) {
-            EntityClassification value = EntityClassification.values()[0];
-            group.addEnum(key, value, e -> save.addProperty(key, e.getString()), NameMap.of(value, EntityClassification.values()).create());
-        }
-    }
+            Set<String> possibleValues = serializer.possibleValues(key, true, provider);
 
-    private static void addReasonConfig(JsonObject save, ConfigGroup group, TestSpawn.Serializer<?> serializer) {
-        for (String key : serializer.propertyKeys()) {
-            SpawnReason value = SpawnReason.values()[0];
-            group.addEnum(key, value, e -> save.addProperty(key, e.name().toLowerCase(Locale.ROOT)), NameMap.of(value, SpawnReason.values()).create());
+            if (possibleValues.size() > 0) {
+                if (serializer instanceof StringLimitSerializer<?, ?>) {
+                    StringLimitSerializer<?, ?> sLS = (StringLimitSerializer<?, ?>) serializer;
+                    List<?> list = possibleValues
+                        .stream()
+                        .map(sLS::fromString)
+                        .collect(Collectors.toList());
+                    if (list.get(0).getClass().isEnum()) {
+                        List<Enum<?>> values = Arrays.asList(((Enum<?>) list.get(0)).getDeclaringClass().getEnumConstants());
+                        group.addEnum(key, values.get(0), e -> save.addProperty(key, e.name().toLowerCase(Locale.ROOT)), NameMap.of(values.get(0), values).create());
+                        continue;
+                    }
+                }
+                List<String> values = possibleValues.stream().sorted().collect(Collectors.toList());
+                group.addEnum(key, "", e -> save.addProperty(key, e), NameMap.of("", values).create());
+            }
         }
     }
 
@@ -67,7 +75,9 @@ public class LMSConfigGui {
         serializer.propertyKeys().stream().sorted(Comparator.naturalOrder())
             .forEach(s -> {
                 int defaultValue = s.equals("maxY") ? 256 : 0;
-                group.addInt(s, defaultValue, i -> save.addProperty(s, i), defaultValue, -limit, limit);
+                int limitMax = s.contains("Y") ? 256 : limit;
+                int limitMin = s.contains("Y") ? 0 : -limit;
+                group.addInt(s, defaultValue, i -> save.addProperty(s, i), defaultValue, limitMin, limitMax);
             });
     }
 }
